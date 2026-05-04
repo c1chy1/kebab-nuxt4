0da554b49a1488837a27f2a7fc3da4dc0d27be72
cichy <cichy08081987pp@gmail.com>
2026-05-04 18:52:18 +0200

    Update `nuxt.config.ts`: add TailwindCSS, Typography plugin, GSAP transpile, image settings, and Brotli compression

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon',"@pinia/nuxt",'@formkit/nuxt'],
+  modules: ['@nuxt/image', '@nuxt/icon','@pinia/nuxt','@formkit/nuxt','@nuxtjs/tailwindcss','@tailwindcss/typography'],
+  formkit: {
+    autoImport: true,
+  },
   vite: {
     optimizeDeps: {
       include: [
         'pinia',
-        'vue3-toastify',
+        'vue3-toastify', // TO DO ANOTHER PACKAGE
       ]
     }
   },
@@ -14,9 +17,21 @@ export default defineNuxtConfig({
     mongodbUri: process.env.MONGODB_URI,
     jwtSecret: process.env.JWTSECRET,
   },
-
+  build: {
+    transpile: ["gsap"],
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.scss',
+    viewer: true,
+  },
+  image: {
+    format: ["webp"],
+  },
   nitro: {
     plugins: ["~~/server/plugins/mongodb.ts"],
+    compressPublicAssets: {
+      brotli: true,
+      gzip: false,
+    },
   },
-
 })
