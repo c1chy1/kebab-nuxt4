c233565598ef6b943ee3e6ecbbd0f078e3e36ffe
cichy <cichy08081987pp@gmail.com>
2026-05-04 18:53:02 +0200

    Add `index.vue` with layout components, user authentication check, and meta configuration

---
+++ app/pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {useUserStore} from "../stores/userStore";
+const user = useUserStore()
+definePageMeta
+({
+  middleware
+      : ["auth"]
+})
+
+useHead({
+  title: 'Kebab',
+})
+
+</script>
+<template >
+  <LayoutNavigation/>
+<!--  <LayoutHeader/>-->
+<!--  <LayoutHome/>-->
+<!--  <LayoutMenu/>-->
+<!--  <LayoutEvents/>-->
+  <LayoutGallery/>
+  <section class="
+  bg-[length:10rem_22rem]
+  sm:bg-[length:18rem_35rem]
+  xl:bg-[length:24rem_40rem]
+  bg-[url('/images/register/register-left.png')] bg-no-repeat">
+    <AccountLogin v-show="!user.isLoggedIn"/>
+    <AccountRegister/>
+  </section>
+
+  <LayoutFooter/>
+
+
+</template>
