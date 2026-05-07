8c535e8ffdd1013b390895b291906871aa93edd4
cichy <cichy08081987pp@gmail.com>
2026-05-07 20:24:58 +0200

    Integrate theme and user management to `app.vue`: add theme toggling, user session handling, and cart loading on lifecycle events.

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,55 @@
-<script setup lang="ts">
-</script>
 <template>
-  <div>
-    <AccountRegister/>
-
+  <div
+      :data-theme="themeStore.theme"
+      class="duration-700 transition-all">
     <main>
       <NuxtPage/>
     </main>
 <!--    <AccountDashboard/>-->
   </div>
 </template>
+<script setup lang="ts">
+import {useThemeStore} from '@/stores/useTheme'
+import {useUserStore} from "@/stores/userStore";
+import {useCartStore} from "@/stores/useCart";
+
+const themeStore = useThemeStore()
+const user = useUserStore()
+
+const {loadCart} = useCartStore()
+
+onBeforeMount(async () => {
+
+  if (user.isLoggedIn) {
+    await user.getMyOrders()
+    await user.me()
+  }
+})
+onMounted(async () => {
+  if (user.isLoggedIn) {
+    await user.getMyOrders()
+    await user.me()
+  }
+  themeStore.theme = localStorage.getItem('daisyui-theme') || 'dark';
+  themeStore.toggleTheme = themeStore.theme === 'dark';
+  if (process.client) {
+    window.scrollTo(0, 0);
+    loadCart()
+  }
+
+});
+
+</script>
+<style>
+.main-body {
+  display: flex;
+  height: calc(100vh - var(--header-height));
+  overflow-x: hidden;
+}
+
+.content {
+  flex-grow: 1;
+  overflow: auto;
+  background-color: var(--background-color);
+}
+</style>
