d28d8b2824da9d59f3f6e3cd064ccc1005926c14
cichy <cichy08081987pp@gmail.com>
2026-05-07 20:23:37 +0200

    Update `nuxt.config.ts`: add Swiper module, smooth scroll behavior, and optimize GSAP dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon','@pinia/nuxt','@formkit/nuxt','@nuxtjs/tailwindcss','@tailwindcss/typography'],
+  modules: ['@nuxt/image', '@nuxt/icon','@pinia/nuxt','@formkit/nuxt','@nuxtjs/tailwindcss','@tailwindcss/typography','nuxt-swiper'],
   formkit: {
     autoImport: true,
   },
+
   vite: {
     optimizeDeps: {
       include: [
         'pinia',
-        'vue3-toastify', // TO DO ANOTHER PACKAGE
+        'vue3-toastify',
+        'gsap/ScrollTrigger',
+        'gsap/Draggable'// TO DO ANOTHER PACKAGE
       ]
     }
   },
-  runtimeConfig: {
-    mongodbUri: process.env.MONGODB_URI,
-    jwtSecret: process.env.JWTSECRET,
+
+  router: {
+    options: {
+      scrollBehaviorType: 'smooth'
+    }
   },
-  build: {
-    transpile: ["gsap"],
+  runtimeConfig: {
+    mongodbUri: '',
+    jwtSecret: '',
+    public: {
+      lightgalleryLicense: '',
+    },
   },
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.scss',
