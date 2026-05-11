97704eed89dd9b783f2af1ccc0d512f5d0732f9a
cichy <cichy08081987pp@gmail.com>
2026-05-11 18:44:02 +0200

    Combine and simplify Toastify imports in `toastify.client.ts`

--- plugins/toastify.client.ts
+++ plugins/toastify.client.ts
@@ -1,6 +1,4 @@
-import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify'
-import 'vue3-toastify/dist/index.css'
-import { toast, type ToastOptions } from 'vue3-toastify'
+import Vue3Toasity, { type ToastContainerOptions, toast, type ToastOptions } from 'vue3-toastify'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(Vue3Toasity) as ToastContainerOptions
